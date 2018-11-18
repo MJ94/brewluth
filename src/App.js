48d4d853d422a9e20c21dd0a55cae2f1e30d9da7
Michael Jackson <mj94@mj94.net>
2018-11-18 01:51:54 -0600

    Grab Foursquare data for breweries in Duluth, Minnesota

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
 
 class App extends Component {
   componentDidMount() {
+    this.getVenues()
     this.loadMap()
   }
 
@@ -11,6 +13,27 @@ class App extends Component {
     const googleMapsKey = process.env.REACT_APP_GOOGLE_MAPS_API_KEY;
     loadScript(`https://maps.googleapis.com/maps/api/js?v=3.exp&key=${googleMapsKey}&callback=initMap`)
     window.initMap = this.initMap;
+  }
+
+  getVenues = () => {
+    const foursquareClientId = process.env.REACT_APP_FOURSQUARE_CLIENT_ID;
+    const foursquareClientSecret = process.env.REACT_APP_FOURSQUARE_CLIENT_SECRET;
+    const endPoint = "https://api.foursquare.com/v2/venues/explore?"
+    const parameters = {
+      client_id: `${foursquareClientId}`,
+      client_secret: `${foursquareClientSecret}`,
+      query: "breweries",
+      near: "Duluth, Minnesota",
+      v: "20181811"
+    }
+
+    axios.get(endPoint + new URLSearchParams(parameters))
+    .then(res => {
+      console.log(res);
+    })
+    .catch((err) => {
+      console.log(err);
+    })
   }
 
   initMap = () => {
