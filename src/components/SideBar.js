8d3cafdd8968c4e4c5ed508db4480bfb19c71732
Michael Jackson <mj94@mj94.net>
2018-11-29 20:23:27 -0600

    Begin filtering venues via search bar

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -2,15 +2,55 @@ import React, { Component } from 'react';
 import VenueList from './VenueList.js';
 
 class Sidebar extends Component {
+  state = {
+    query: ""
+  }
+
+  filterSidebarBreweries = () => {
+    if (this.state.query.trim !== "") {
+      const breweries = this.props.breweries.filter(breweryListing => {
+        breweryListing.venue.name.toLowerCase().includes(this.state.query.toLowerCase())
+      })
+      return breweries;
+    }
+    return this.props.venues;
+  }
+
+  handleUpdate = (e) => {
+    this.setState({
+      query: e.target.value
+    });
+    const markers = this.props.breweries.map(breweries => {
+      const firstMatch = breweries.venue.name.toLowerCase().includes(e.target.value.toLowerCase());
+      const marker = this.props.markers.find(
+        (marker) => marker.id === breweries.venue.id
+      );
+      if (firstMatch) {
+        marker.setVisible(true);
+        marker.setAnimation(window.google.maps.Animation.BOUNCE);
+        setTimeout(function(){ marker.setAnimation(null); }, 750);
+      } else {
+        marker.setVisible(false);
+      }
+      return markers;
+    });
+  };
+
   render() {
     return (
       <aside id="sideBarContainer">
         <h1 id="sideBarHeader">Duluth, MN</h1>
         <label>Search Breweries</label>
-        <input id="filter" type="search" placeholder="Brewery"></input>
+        <input
+          id="filter"
+          type="search"
+          placeholder="Brewery"
+          onChange={this.handleUpdate}
+        />
         <VenueList
           {...this.props}
           whenSideBarBreweryClicked={this.props.whenSideBarBreweryClicked}
+          breweryQuery={this.filterSidebarBreweries()}
           />
       </aside>
     );
